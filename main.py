ee1d331b49ac9d40f60e4fd8605bee4139359a7a
aditya <extracorrection@gmail.com>
2021-09-14 05:16:59 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import pyttsx3
+import PyPDF2
+from PyPDF2 import PdfFileReader
+
+#for speak function
+engine=pyttsx3.init("sapi5")
+#sapi5 helps to enhance sppech-text recognition
+#get some properties of pyttsx3
+voices=engine.getProperty('voices')
+engine.getProperty('rate')
+engine.getProperty('volume')
+#change the rate of reading a/q to you  
+# suggestion : put it <150
+engine.setProperty('rate', 165)
+#this changes the volume level 
+engine.setProperty('volume',200 )
+engine.setProperty('voices' , voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def PDFreader(file_path):
+    # Creating a pdf file object.
+    pdf = open(file_path, "rb")
+    # Creating pdf reader object.
+    pdf_reader = PyPDF2.PdfFileReader(pdf)
+    # Checking total number of pages in a pdf file.
+    total_pages = pdf_reader.numPages
+    #loop for reading the file from 4th page to last
+    for page_no in range(4,total_pages):
+        page=pdf_reader.getPage(page_no)
+        text=page.extractText()
+        print(text)
+        speak(text)
+    pdf.close()
+file_path=input("Enter the full path of file : ")  
+if file_path.endswith('.pdf'):
+    PDFreader(file_path)
+
+else:
+    print("This file type is unsupportable")
+    speak("This file type is unsupportable")
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Audiobook 
+  This is a simple python projects which aims to read the content of any PDF.
+## Installation
+After clonning this repo you need to install some packages for this project.Run this commands in your terminal to install :)
+
+```bash
+pip install -r requirements.txt
+```
+## Support 
+For any help regarding this repo :     [DM here ](instagram.com/codefever.exe/)       
+and for reference to this project : [Read this post ](instagram.com/p/CSOwKV_DYwW/)
